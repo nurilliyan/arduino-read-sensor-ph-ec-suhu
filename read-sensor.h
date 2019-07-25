88278c87a670b181cdf195995a7208d808850d76
nurilliyan <nurilliyansuryagopur@gmail.com>
2019-07-25 10:01:34 +0700

    Inisialisasi dan deklarasi variabel untuk pembacaan sensor pH

--- read-sensor.h
+++ read-sensor.h
@@ -1,3 +1,21 @@
+/*
+ * Deklarasi pH
+ * Digunakan untuk menentukan tipe variabel dan penentuan pin input analog pH
+ */
+#define Offset 0.14                      //Nilai penyimpangan
+#define SensorPin A0                     //Pin Analog 0 untuk input data sensor pH meter SEN0161
+#define pHsamplingInterval 25            //Interval waktu pembacaan analog sampel pH tiap 25 mili detik
+#define pHprintInterval 1000             //Nilai pH di print out atau ditampilkan setiap 1000 mili detik
+#define ArrayLenth  40                   //Jumlah maksimal panjang data [index] didalam array 
+unsigned int pHArray[ArrayLenth];        //Hasil nilai rata-rata analog yang masuk dari sensor
+byte pHArrayIndex=0;                     //Index pembacaan analog pH mulai dari index pertama [0]
+unsigned long pHsamplingTime;            //Deklarasi variabel pHsamplingTime
+unsigned long pHprintTime;               //Deklarasi variabel pHprintTime
+float pHvoltage;                         //Tipe data untuk hasil tegangan pH berupa float
+float pHValue;                           //Tipe data untuk hasil nilai pH berupa float
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
